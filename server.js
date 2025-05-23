5cfd69490ccdc3d91798596a9953b6bae5185de2
Temi <temiloluwaodunuga13@gmail.com>
2025-05-23 21:09:04 +0100

    my first github push in class-26/05/25

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Middleware to parse JSON
+app.use(express.json());
+
+
+// Define routes
+app.get('/', (req, res) => {
+  res.send('you are now in a different territory');
+});
+
+
+app.get('/me', (req, res) => {
+  res.send('now that you are here, this is owned by me Temiloluwa the greatest person alive!!!');
+
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
